101d7ccdbd0711fd173292fb2c84dacb98313bb7
hudhyfa <hudhyfahbk@gmail.com>
2026-01-01 17:56:11 +0530

    day 11 JS types and variables pracitce

---
+++ js/js1.js
@@ -0,0 +1,16 @@
+console.log('hello')
+console.log('im beggining js')
+document.getElementById("js1").innerHTML = 'Hello';
+let age = 15;
+console.log(`She is ${age} years old ... Benzemaaaa...`);
+let y,z,a;
+y= 19;
+z= 200;
+a = z-y;
+let ans = [{name:"ismail"},"5","6"]
+console.log(typeof(ans[0]))
+document.getElementById("myP").innerHTML = "value of 'a' is: " + a;
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+
+console.log(id1 === id2);
